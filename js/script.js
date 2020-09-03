79b99c4ff9445d8a8d747b8a0f4dea6b65b251f2
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2020-09-03 16:56:21 +0300

    start

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// === подключение библиотек === //
+//преобразуем селекты библиотека https://select2.org/
+$(document).ready(function() {
+  $('.form__select').select2({
+  });
+});
+// === главная страница === //
+// открывание мобильного меню
+$('.header__mobile-btn').click(function () {
+  $('.header__navigation').slideToggle();
+  $('.header__user-list').slideToggle();
+  $('.header__user-list').css('display', 'flex');
+  $('.header__user').css('order', '1')
+  // $('.header__settings').slideToggle();
+  // $('.header__user-block').slideToggle();
+  if ($(this).hasClass('header__mobile-btn--gamburger')) {
+    $(this).removeClass('header__mobile-btn--gamburger');
+    $(this).addClass('header__mobile-btn--close')
+  } else if ($(this).hasClass('header__mobile-btn--close')) {
+    $(this).removeClass('header__mobile-btn--close');
+    $(this).addClass('header__mobile-btn--gamburger');
+    $('.header__user').removeAttr('style');
+    $('.header__user-list').slideOut();
+    $('.header__user-list').removeAttr('style');
+  }
+});
+// открывание меню залогиненого юзера
+$('.header__logged-icon-img').click(function () {
+  $('.header__logged-name').slideToggle();
+  $('.header__logged-list').slideToggle();
+  $('.header__logged-icon').css('display', 'block');
+});
+// открывание фильтров на мобильной версии
+$('.present__form-filter-mobile-btn').click(function() {
+  $('.present__form').addClass('present__form--active');
+  $(this).css('display', 'none');
+});
+// закрытие фильтров на мобильной версии
+$('.present__form-btn-close').click(function() {
+  if ($('.present__form').hasClass('present__form--active')){
+    $('.present__form').removeClass('present__form--active');
+    $('.present__form-filter-mobile-btn').removeAttr('style');
+  }
+});
