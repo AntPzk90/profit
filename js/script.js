dd06477f125c72de78574eceeda5479f771276aa
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2020-09-04 16:24:44 +0300

    add animation on index

--- js/script.js
+++ js/script.js
@@ -23,21 +23,23 @@ $('.header__mobile-btn').click(function () {
     $(this).removeClass('header__mobile-btn--close');
     $(this).addClass('header__mobile-btn--gamburger');
     $('.header__user').removeAttr('style');
-    $('.header__user-list').slideOut();
+    $('.header__user-list').slideUp();
     $('.header__user-list').removeAttr('style');
   }
 });
 // открывание меню залогиненого юзера
 $('.header__logged-icon-img').click(function () {
   $('.header__logged-name').slideToggle();
-  $('.header__logged-list').slideToggle();
+  $('.header__logged-list').slideToggle(500);
   $('.header__logged-icon').css('display', 'block');
 });
+
 // открывание фильтров на мобильной версии
 $('.present__form-filter-mobile-btn').click(function() {
   $('.present__form').addClass('present__form--active');
   $(this).css('display', 'none');
   $('.present__form-wrapper').css('padding-bottom', '0');
+  $('html, body').animate({scrollTop: 0},300);
 });
 // закрытие фильтров на мобильной версии
 $('.present__form-btn-close').click(function() {
